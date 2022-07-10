833d0f3fc6d793a0d4037cc5264f65e351d121ae
Tom Morgan <thomas.morgan1903@gmail.com>
2022-07-10 09:47:28 +0100

    booking app inintial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from '../theme/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ theme/theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: blue,
+  },
+  typography: {
+    fontFamily: [
+      'Inter',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
